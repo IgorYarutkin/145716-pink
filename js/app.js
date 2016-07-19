d93917f15ef5a8c64fe63dbe06ae854581f5a507
IgorYarutkin <igor.yarutkin@gmail.com>
2016-07-19 19:12:31 +0300

    Сделал меню при выключенном js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 "use strict";
 
 (function() {
+  var pageHeader = document.querySelector('.page-header');
   var pageHeaderTop = document.querySelector(".page-header__top");
+  var pageHeaderTopContainer = document.querySelector('.page-header__top-container');
+  var pageHeaderContainer = document.querySelector('.page-header__container');
   var menu = document.querySelector(".menu");
   var menuButton = document.querySelector(".page-header__button");
 
   var onStart = function() {
     pageHeaderTop.classList.remove("page-header__top--no-js");
     menu.classList.remove("menu--no-js");
+    pageHeader.classList.remove('page-header--no-js');
+    pageHeaderTopContainer.classList.remove('page-header__top-container--no-js');
+    pageHeaderContainer.classList.remove('page-header__container--no-js');
   }
 
   var checkButton = function() {
     pageHeaderTop.classList.toggle("page-header__top--menu-open");
     menu.classList.toggle("menu--open");
+    menuButton.classList.toggle("page-header__button--menu-open");
   }
 
   onStart();
